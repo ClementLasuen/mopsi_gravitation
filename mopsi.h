8bd692238bb6559b7059a6b76f3ec4d07dc348d1
qspinat <43143668+qspinat@users.noreply.github.com>
2018-11-03 15:02:34 +0100

    Add files via upload

--- mopsi.h
+++ mopsi.h
@@ -12,7 +12,11 @@ const int nb_iterations = 100;
 const float m [nb_planetes] = {1.0,1.0};
 const int G =1;
 
+
+
 FVector<FVector<float,3>,nb_planetes> f();
+
+// ------------------------------------------- Fonctions pratiques ----------------------------------------
 
 float norme(FVector<float,3> v);
 FVector<FVector<float,3>,nb_planetes> interaction(FVector<FVector<float,3>,nb_planetes> q);
@@ -22,4 +26,6 @@ FVector<FVector<float,3>,nb_planetes>* point_fixe(FVector<FVector<float,3>,nb_pl
 
 float ecart(FVector<FVector<float,3>,nb_planetes> q0, FVector<FVector<float,3>,nb_planetes> q1);
 
-FVector<FVector<float,3>,nb_planetes>* euler_implicite(FVector<FVector<float,3>,nb_planetes> q, FVector<FVector<float,3>,nb_planetes> p0);
+//---------------------------------- Methodes d'integration -------------------------------------------
+
+FVector<FVector<float,3>,nb_planetes>* euler_implicite(FVector<FVector<float,3>,nb_planetes> q, FVector<FVector<float,3>,nb_planetes> p0, float masse);
