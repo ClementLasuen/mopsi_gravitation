fc96332d6fbff5d191c8d5031ab7455eef340799
Quentin Spinat <qspinat@gmail.com>
2018-11-11 18:32:25 +0100

    rajout des commentaires

--- ecriture.h
+++ ecriture.h
@@ -6,3 +6,8 @@
 using namespace std;
 
 void ecriture(string file_name, FVector<FVector<double, _Tp2>, nb_planetes> *trajectory);
+/* ecrit un fichier avec pour premiere ligne le nombre d'iterations puis le nombre de planetes
+   En deuxieme ligne il y a la ligne de toute les planetes
+   Vient ensuite n_iterations lignes avec les coordonnes de toutes les planetes les unes a la suite des autres (donc nb_iterations lignes de 3*nb_planete nombres)
+   Vient ensuite n_iterations lignes avec les VITESSES de toutes les planetes les unes a la suite des autres (donc nb_iterations lignes de 3*nb_planete nombres)
+*/
