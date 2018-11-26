3135d413cbad637f8c11c015ed55d2b6016107fc
Quentin Spinat <qspinat@gmail.com>
2018-11-26 14:24:29 +0100

    bug

--- mopsi.h
+++ mopsi.h
@@ -9,8 +9,8 @@ using namespace std;
 using namespace Imagine;
 
 const int nb_planetes =5;
-const double h = 1;
-const int nb_iterations = 100000;
+const double h = 10;
+const int nb_iterations = 200000;
 const double m [nb_planetes] = {1.00000597682,0.0009548, 0.00029, 0.0000437, 0.0000518};
 const double G =2.959122*0.0001;
 
