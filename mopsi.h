dd1a1a8fd7584b0bf8e8137a280e52e63d4690ad
ClementLasuen <36895029+ClementLasuen@users.noreply.github.com>
2019-01-20 18:10:43 +0100

    Update mopsi.h

--- mopsi.h
+++ mopsi.h
@@ -14,6 +14,35 @@ const int nb_iterations = 200000;
 const double m [nb_planetes] = {1.00000597682,0.0009548, 0.00029, 0.0000437, 0.0000518};
 const double G =2.959122*0.0001;
 
+// -------------------------------------------- Initialisation -------------------------------------------
+
+// Initialisation
+
+// Soleil
+const FVector<double,3> q_soleil = {0.0,0.0,0.0};
+const FVector<double,3> p_soleil = {0.0,0.0,0.0};
+
+// Jupiter
+
+const FVector<double,3> q_jupiter = {-3.5024 , -3.8170 , -1.5508};
+const FVector<double,3> p_jupiter1 = {0.0056543 , -0.0041249, -0.0019059};
+const FVector<double,3> p_jupiter = m[1]*p_jupiter1;
+
+// Saturne
+
+const FVector<double,3> q_saturne = {9.1,-3.0,-1.6};
+const FVector<double,3> p_saturne1 = {0.0017, 0.0048,0.0019};
+const FVector<double,3> p_saturne = m[2]*p_saturne1;
+
+// Uranus
+const FVector<double,3> q_uranus = {8.3101, -16.290, -7.2521};
+const FVector<double,3> p_uranus1 = {0.0035418, 0.0013710, 0.00055029};
+const FVector<double,3> p_uranus = m[3]*p_uranus1;
+// Neptune
+
+const FVector<double,3> q_neptune = {-15.539, -25.223, -3.1902};
+const FVector<double,3> p_neptune1 = {0.0028893, -0.0017070, -0.0013650};
+const FVector<double,3> p_neptune = m[4]*p_neptune1;
 // ------------------------------------------- Fonctions pratiques ----------------------------------------
 
 double norme(FVector<double,3> v);
@@ -33,7 +62,7 @@ double H_modifie_V(FVector<FVector<double,3>,nb_planetes> q, FVector<FVector<dou
 FVector<FVector<double, 3*nb_planetes>,3*nb_planetes > Hessienne2(FVector<FVector<double, 3>, nb_planetes> q);
 
 
-FVector<FVector<double, 3*nb_planetes>,3*nb_planetes >  Hessienne(FVector<FVector<double,3>,nb_planetes> q, FVector<FVector<double,3>,nb_planetes> p);
+FVector<FVector<double, 3*nb_planetes>,3*nb_planetes >  Hessienne(FVector<FVector<double,3>,nb_planetes> q);
 //---------------------------------- Methodes d'integration -------------------------------------------
 
 // Renvoie une "distance" entre les positions q0 et q1
