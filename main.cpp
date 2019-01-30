c13b3f68f8b9d7b8e504828116afc8ecac85c958
ClementLasuen <36895029+ClementLasuen@users.noreply.github.com>
2019-01-30 16:36:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 
 #include "mopsi.h"
-#include</home/quentin/anaconda3/include/python3.5m/Python.h>
 
+#include "stdlib.h"
 
-int main()
-{
-        double h;
+int main(){
+
+    double h;
     string methode;
 
     cout << "Quel est le pas de temps ? " << endl;
@@ -13,18 +13,20 @@ int main()
 
 
     cout << "Quelle methode numerique a utiliser ?" << endl;
-    cout << "explicite, implicite, symplectique1,  symplectique2, Verlet" << endl;
+    //cout << "explicite, implicite, symplectique1,  symplectique2, Verlet" << endl;
+    cout << "ES ou V" << endl;
     cin >> methode ;
     //FVector<FVector<double,3>,nb_planetes>* resu;
     bool choix_methode = true;
-    if(methode == "explicite")  euler_explicite(h);
-    /*else if(methode == "implicite")  resu = euler_implicite(h);
-    else if(methode == "symplectique1")  resu = euler_symplectique(h);*/
-    else if(methode == "symplectique2") euler_symplectique_sans_pf(h);
-    else if(methode == "Verlet")  verlet(h);
+    if(methode == "ES") euler_symplectique(h);
+    else if(methode == "V")  verlet(h);
     else{
         choix_methode=false;
         cout << "choisis une methode" << endl;
     }
+
+
+    //system(".\python3.4 test_hess.py");
+
     return 0;
 }
